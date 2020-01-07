97ae6bd6f8f8ab1671f41944720c5e8c4dc1dc6d
Gaykhe Sachin Pandharinath <gaykhesachin5@gmail.com>
2020-01-07 18:29:57 +0530

    Area of Pentagon

---
+++ areaOfPentagon.js
@@ -0,0 +1,8 @@
+function areaOfPentagon(side,length){
+       
+    
+    var area_pentagon=(5.0/2.0)*side*length;  
+    console.log("Area of the pentagon = "+area_pentagon);   
+    
+}
+areaOfPentagon(13,5);
