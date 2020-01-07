81ce35b1430b30451b761daf8da7396293c4d164
Gaykhe Sachin Pandharinath <gaykhesachin5@gmail.com>
2020-01-07 18:37:26 +0530

    Factorial Number

---
+++ Factorial.js
@@ -0,0 +1,11 @@
+function Factorial(number1){
+    var fact=1;
+    var i;
+    for(i=1;i<=number1;i++)
+    {
+    fact=fact*i;
+    }
+    console.log("Factorial of "+number1+" number is = "+fact);
+}	
+Factorial(4);	
+Factorial(5);
