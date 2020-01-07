3e594bbfbb82816e8f9512214bde2c22d469942d
Gaykhe Sachin Pandharinath <gaykhesachin5@gmail.com>
2020-01-07 19:01:56 +0530

    Volume of Sphere

---
+++ volumeOfSphere.js
@@ -0,0 +1,8 @@
+function volumeOfSphere(radius){
+    var pie=3.14;  
+    var volume=(4.0/3.0)*pie*(radius*radius*radius);  
+    console.log("Volume of the sphere= " +volume);  
+    
+}
+volumeOfSphere(2);
+volumeOfSphere(5);
